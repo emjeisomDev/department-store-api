f61ef0931adb1fddc75b4015e334209bfb109ce9
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-05-08 12:26:13 -0300

    f0-e1: spring-initializr-setup

---
+++ src/main/java/com/departmentstore/api/DepartmentStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.departmentstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/departmentstore/api/DepartmentStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departmentstore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
